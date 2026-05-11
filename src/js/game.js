4ae0cbfdecd050a52463a7d72ba13cd6e75cf3ec
Efe <efetwkin@gmail.com>
2026-05-11 15:51:10 +0200

    add shark and background

--- src/js/game.js
+++ src/js/game.js
@@ -15,17 +15,40 @@ export class Game extends Engine {
     }
 
     startGame() {
-        console.log("start de game!")
+
+    console.log("start de game!")
+
+        const background = new Actor({
+        pos: new Vector(640, 360)
+        })
+        background.graphics.use(Resources.Background.toSprite())
+        this.add (background)
+
+        
         const fish = new Actor()
         fish.graphics.use(Resources.Fish.toSprite())
         fish.pos = new Vector(500, 300)
         fish.vel = new Vector(-10,0)
         fish.events.on("exitviewport", (e) => this.fishLeft(e))
         this.add(fish)
+
+        const shark = new Actor()
+        shark.graphics.use(Resources.Shark.toSprite())
+        shark.pos = new Vector(900, 200)
+        shark.vel = new Vector(15,0)
+        shark.events.on("exitviewport", (e) => this.sharkLeft(e))
+        this.add(shark)
+
+            
+       
     }
 
     fishLeft(e) {
         e.target.pos = new Vector(1350, 300)
+    }
+
+    sharkLeft(e) {
+        e.target.pos = new Vector(0, 500)
     }
 }
 
